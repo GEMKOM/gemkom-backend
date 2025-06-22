d43738289be9dc8078687160059181ac906e11f6
onatcalik-gemkom <ocalik@gemkom.com.tr>
2025-06-22 10:54:17 +0300

    serializer changed

--- machining/serializers.py
+++ machining/serializers.py
@@ -4,4 +4,23 @@ from .models import Timer
 class TimerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Timer
-        fields = '__all__'
+        fields = [
+            'id',
+            'user',  # required by model, but will be auto-assigned
+            'issue_key',
+            'start_time',
+            'finish_time',
+            'synced_to_jira',
+            'comment',
+            'machine',
+            'job_no',
+            'image_no',
+            'position_no',
+            'quantity',
+            'manual_entry',
+        ]
+        read_only_fields = ['id', 'user']  # don't allow frontend to set user
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
--- machining/views.py
+++ machining/views.py
@@ -9,10 +9,10 @@ class TimerStartView(MachiningProtectedView):
     def post(self, request):
         data = request.data.copy()
         data["manual_entry"] = False
-        serializer = TimerSerializer(data=data)
+        serializer = TimerSerializer(data=data, context={'request': request})
         if serializer.is_valid():
-            serializer.save()
-            return Response({"id": serializer.data['id']}, status=200)
+            timer = serializer.save()
+            return Response({"id": timer.id}, status=200)
         return Response(serializer.errors, status=400)
 
 class TimerStopView(MachiningProtectedView):
@@ -20,9 +20,14 @@ class TimerStopView(MachiningProtectedView):
         timer_id = request.data.get("timer_id")
         try:
             timer = Timer.objects.get(id=timer_id)
+
+            if timer.user != request.user:
+                return Response("Permission denied for this timer.", status=403)
+
             for field in ['finish_time', 'comment', 'synced_to_jira', 'machine']:
                 if field in request.data:
                     setattr(timer, field, request.data[field])
+
             timer.save()
             return Response("Timer stopped and updated.", status=200)
         except Timer.DoesNotExist:
@@ -32,21 +37,29 @@ class TimerManualEntryView(MachiningProtectedView):
     def post(self, request):
         data = request.data.copy()
         data["manual_entry"] = True
-        serializer = TimerSerializer(data=data)
+        serializer = TimerSerializer(data=data, context={'request': request})
         if serializer.is_valid():
-            serializer.save()
-            return Response({"id": serializer.data['id']}, status=200)
+            timer = serializer.save()
+            return Response({"id": timer.id}, status=200)
         return Response(serializer.errors, status=400)
 
 class TimerListView(MachiningProtectedView):
     def get(self, request):
         query = Q()
+
+        # Optional filtering by active/inactive timers
         if request.GET.get("is_active") == "true":
             query &= Q(finish_time__isnull=True)
         elif request.GET.get("is_active") == "false":
             query &= Q(finish_time__isnull=False)
-        if "user" in request.GET:
-            query &= Q(user__username=request.GET["user"])
+
+        # Handle user filtering
+        user_param = request.GET.get("user")
+        if user_param and (request.user.is_superuser or getattr(request.user, "is_admin", False)):
+            query &= Q(user__username=user_param)
+        else:
+            query &= Q(user=request.user)
+
         if "issue_key" in request.GET:
             query &= Q(issue_key=request.GET["issue_key"])
 
