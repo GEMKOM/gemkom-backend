318d7390376b0b0dc18ebbecd97a70f180395ab3
onatcalik-gemkom <ocalik@gemkom.com.tr>
2025-06-22 11:14:40 +0300

    authentication updated

--- machining/permissions.py
+++ machining/permissions.py
@@ -2,6 +2,8 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.views import APIView
 from users.permissions import IsAdmin, IsMachiningUser  # your custom permissions
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework.exceptions import AuthenticationFailed
 
 class MachiningProtectedView(APIView):
     authentication_classes = [JWTAuthentication]  # Ensure JWT auth is applied
@@ -9,7 +11,10 @@ class MachiningProtectedView(APIView):
     def dispatch(self, request, *args, **kwargs):
         # Run authentication manually
         for authenticator in self.authentication_classes:
-            auth_result = authenticator().authenticate(request)
+            try:
+                auth_result = authenticator().authenticate(request)
+            except (InvalidToken, TokenError) as e:
+                raise AuthenticationFailed("Token is expired or invalid.")
             if auth_result is not None:
                 request.user, request.auth = auth_result
                 break
