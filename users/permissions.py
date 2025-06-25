aac69aa00a980ee4c5e285a8349fc9f36e92fef9
onatcalik-gemkom <ocalik@gemkom.com.tr>
2025-06-25 23:16:27 +0300

    Update permissions.py

--- users/permissions.py
+++ users/permissions.py
@@ -3,13 +3,15 @@ from rest_framework.permissions import BasePermission
 class IsMachiningUserOrAdmin(BasePermission):
     def has_permission(self, request, view):
         user = request.user
+        profile = getattr(user, "profile", None)
+
         return (
             user
             and user.is_authenticated
             and (
                 user.is_superuser
-                or getattr(user, "is_admin", False)
-                or getattr(user, "team", "").lower() == "machining"
+                or getattr(profile, "is_admin", False)
+                or getattr(profile, "team", "").lower() == "machining"
             )
         )
 
