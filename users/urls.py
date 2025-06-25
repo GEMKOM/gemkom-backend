8fee9d1c1b8d766210b0fa0e783b7fb1af2cb23b
onatcalik-gemkom <ocalik@gemkom.com.tr>
2025-06-25 20:36:32 +0300

    user info added

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserListView
+from .views import CurrentUserView, UserListView
 
 urlpatterns = [
     path('', UserListView.as_view(), name='user-list'),
+    path("me/", CurrentUserView.as_view(), name="current-user"),
 ]
--- users/views.py
+++ users/views.py
@@ -2,11 +2,19 @@ from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserListSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class UserListView(APIView):
-    # permission_classes = [IsAuthenticated]  # Optional: Require login
+    permission_classes = [IsAuthenticated]  # Optional: Require login
 
     def get(self, request):
         users = User.objects.all().select_related('profile')
         serializer = UserListSerializer(users, many=True)
+        return Response(serializer.data)
+
+class CurrentUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserListSerializer(request.user)
         return Response(serializer.data)
