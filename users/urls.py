c30130acfa3facd2cd6e62f8879a9b9e4e4ad770
onatcalik-gemkom <ocalik@gemkom.com.tr>
2025-06-27 16:12:47 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import AdminCreateUserView, CurrentUserView, ForcedPasswordResetView, TeamChoicesView, UserListView
+from .views import AdminBulkCreateUsers, AdminCreateUserView, CurrentUserView, ForcedPasswordResetView, TeamChoicesView, UserListView
 
 urlpatterns = [
     path('', UserListView.as_view(), name='user-list'),
     path("me/", CurrentUserView.as_view(), name="current-user"),
     path('admin/create-user/', AdminCreateUserView.as_view(), name='admin-create-user'),
+    path('admin/bulk-create-user/', AdminBulkCreateUsers.as_view(), name='admin-create-user-bulk'),
     path("reset-password/", ForcedPasswordResetView.as_view(), name="forced-password-reset"),
     path("teams/", TeamChoicesView.as_view(), name="teams-list"),
 ]
