8e3485f0b4e63cc2795102f23524bf9d47088a3a
onatcalik-gemkom <ocalik@gemkom.com.tr>
2025-07-04 11:54:38 +0300

    Update filters.py

--- machining/filters.py
+++ machining/filters.py
@@ -8,9 +8,9 @@ class TaskFilter(django_filters.FilterSet):
     completion_date__isnull = django_filters.BooleanFilter(field_name='completion_date', lookup_expr='isnull')
 
     completion_date = django_filters.DateFilter(field_name='completion_date')  # exact match
-    completion_date__gte = django_filters.DateFilter(field_name='completion_date', lookup_expr='gte')  # after or on
-    completion_date__lte = django_filters.DateFilter(field_name='completion_date', lookup_expr='lte')  # before or on
+    completion_date__gte = django_filters.NumberFilter(field_name='completion_date', lookup_expr='gte')  # after or on
+    completion_date__lte = django_filters.NumberFilter(field_name='completion_date', lookup_expr='lte')  # before or on
 
     class Meta:
         model = Task
-        fields = ['key', 'name', 'job_no', 'position_no', 'image_no', 'completed_by', 'completion_date']
+        fields = ['key', 'name', 'job_no', 'position_no', 'image_no', 'completed_by', 'completion_date', 'completion_date__gte', 'completion_date__lte']
