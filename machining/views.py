c4182e7e6923ba9fadc0b4f4fbb18e96b312e355
onatcalik-gemkom <ocalik@gemkom.com.tr>
2025-07-14 15:27:12 +0300

    Update views.py

--- machining/views.py
+++ machining/views.py
@@ -26,6 +26,7 @@ class TimerStartView(MachiningProtectedView):
             return Response({"id": timer.id}, status=200)
         return Response(serializer.errors, status=400)
 
+# API View for maintenance to be able to stop maintenance timers.
 class TimerStopView(APIView):
     def post(self, request):
         timer_id = request.data.get("timer_id")
