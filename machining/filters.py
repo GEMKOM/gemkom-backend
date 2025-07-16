61e6f1626ea18fef16fa10dfb839ad756af23ac1
onatcalik-gemkom <ocalik@gemkom.com.tr>
2025-07-16 14:17:09 +0300

    Update filters.py

--- machining/filters.py
+++ machining/filters.py
@@ -7,10 +7,29 @@ class TaskFilter(django_filters.FilterSet):
     job_no = django_filters.CharFilter(lookup_expr='icontains')
     completion_date__isnull = django_filters.BooleanFilter(field_name='completion_date', lookup_expr='isnull')
 
-    completion_date = django_filters.DateFilter(field_name='completion_date')  # exact match
-    completion_date__gte = django_filters.NumberFilter(field_name='completion_date', lookup_expr='gte')  # after or on
-    completion_date__lte = django_filters.NumberFilter(field_name='completion_date', lookup_expr='lte')  # before or on
+    completion_date = django_filters.DateFilter(field_name='completion_date')
+    completion_date__gte = django_filters.NumberFilter(field_name='completion_date', lookup_expr='gte')
+    completion_date__lte = django_filters.NumberFilter(field_name='completion_date', lookup_expr='lte')
+
+    finish_time = django_filters.NumberFilter(field_name='finish_time')  # ✅
+    finish_time__gte = django_filters.NumberFilter(field_name='finish_time', lookup_expr='gte')  # ✅
+    finish_time__lte = django_filters.NumberFilter(field_name='finish_time', lookup_expr='lte')  # ✅
 
     class Meta:
         model = Task
-        fields = ['key', 'name', 'job_no', 'position_no', 'image_no', 'completed_by', 'completion_date', 'completion_date__gte', 'completion_date__lte', 'machine_fk']
+        fields = [
+            'key',
+            'name',
+            'job_no',
+            'position_no',
+            'image_no',
+            'completed_by',
+            'completion_date',
+            'completion_date__gte',
+            'completion_date__lte',
+            'finish_time',
+            'finish_time__gte',
+            'finish_time__lte',
+            'machine_fk',
+        ]
+
