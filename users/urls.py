226b26c32f018b7b6b890b4f4232b1b0e2641a22
onatcalik-gemkom <ocalik@gemkom.com.tr>
2025-07-21 10:23:40 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AdminBulkCreateUsers, AdminCreateUserView, CurrentUserView, ForcedPasswordResetView, TeamChoicesView, UserListView, UserViewSet
+from .views import AdminBulkCreateUsers, CurrentUserView, ForcedPasswordResetView, TeamChoicesView, UserListView, UserViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -7,7 +7,6 @@ router.register(r'', UserViewSet, basename='user')
 
 urlpatterns = [
     path("me/", CurrentUserView.as_view(), name="current-user"),
-    path('admin/create-user/', AdminCreateUserView.as_view(), name='admin-create-user'),
     path('admin/bulk-create-user/', AdminBulkCreateUsers.as_view(), name='admin-create-user-bulk'),
     path("reset-password/", ForcedPasswordResetView.as_view(), name="forced-password-reset"),
     path("teams/", TeamChoicesView.as_view(), name="teams-list"),
