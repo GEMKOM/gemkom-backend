4f47954f8faedbbd284926bb989b41daf0b0c4e5
onatcalik-gemkom <ocalik@gemkom.com.tr>
2025-07-21 11:52:13 +0300

    Update filters.py

--- users/filters.py
+++ users/filters.py
@@ -1,8 +1,11 @@
 from django_filters import rest_framework as filters
 from django.contrib.auth.models import User
 
+class CharInFilter(filters.BaseInFilter, filters.CharFilter):
+    pass
+
 class UserFilter(filters.FilterSet):
-    team = filters.CharFilter(field_name='profile__team')
+    team = CharInFilter(field_name='profile__team', lookup_expr='in')
     is_admin = filters.BooleanFilter(field_name='profile__is_admin')
     is_lead = filters.BooleanFilter(field_name='profile__is_lead')
     must_reset_password = filters.BooleanFilter(field_name='profile__must_reset_password')
