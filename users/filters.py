71df25a7e025ecc01136423027281b050cb87f76
onatcalik-gemkom <ocalik@gemkom.com.tr>
2025-08-29 15:46:40 +0300

    Update filters.py

--- users/filters.py
+++ users/filters.py
@@ -1,16 +1,20 @@
-from django_filters import rest_framework as filters
 from django.contrib.auth.models import User
+from django_filters import rest_framework as filters
+from django_filters.filters import BaseInFilter, CharFilter
 
-class CharInFilter(filters.BaseInFilter, filters.CharFilter):
+class CharInFilter(BaseInFilter, CharFilter):
+    """Accepts comma-separated values, e.g. ?team=machining,design"""
     pass
 
 class UserFilter(filters.FilterSet):
+    # substring match anywhere in username (on, onat, nat, cali…)
+    username = filters.CharFilter(field_name='username', lookup_expr='icontains')
+
+    # allow single or multi select via comma-separated values
     team = CharInFilter(field_name='profile__team', lookup_expr='in')
-    is_admin = filters.BooleanFilter(field_name='profile__is_admin')
-    is_lead = filters.BooleanFilter(field_name='profile__is_lead')
-    must_reset_password = filters.BooleanFilter(field_name='profile__must_reset_password')
-    work_location = filters.CharFilter(field_name='profile__work_location')
+    work_location = CharInFilter(field_name='profile__work_location', lookup_expr='in')
+    occupation = CharInFilter(field_name='profile__occupation', lookup_expr='in')
 
     class Meta:
         model = User
-        fields = ['team', 'is_admin', 'is_lead', 'must_reset_password', 'work_location']
+        fields = ['username', 'team', 'work_location', 'occupation']
